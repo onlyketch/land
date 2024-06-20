b280431196456adf74b43b83884db438540da121
Andrey <byketch@yandex.ru>
2024-06-20 15:58:16 +0300

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    
+    $('.warehouses__slider').slick({
+        arrows: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+    });
+
+    $('.gallery__slider').slick({
+        arrows: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+    });
+    
+});
