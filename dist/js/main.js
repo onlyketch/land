a7421e2c8b61ba3bf06ffcf32d94610eb77256ba
Andrey <byketch@yandex.ru>
2024-06-21 13:53:20 +0300

    adaptive ready

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,12 +4,59 @@ $(document).ready(function() {
         arrows: true,
         slidesToShow: 3,
         slidesToScroll: 1,
+        responsive:[
+            {
+              breakpoint: 960,
+              settings: {
+                slidesToShow: 2
+              }  
+            },
+            {
+                breakpoint: 770,
+                settings: {
+                  slidesToShow: 1
+                }  
+              },
+        ]
     });
 
     $('.gallery__slider').slick({
         arrows: true,
         slidesToShow: 2,
         slidesToScroll: 1,
+        responsive:[
+            {
+              breakpoint: 960,
+              settings: {
+                slidesToShow: 1
+              }  
+            }
+        ]
+    });
+
+    $('.choose-btn').on('click', function(e){
+        $('html,body').stop().animate({ scrollTop: $('#warehouses').offset().top }, 10);
+        e.preventDefault();
+    });
+
+    let burg = document.getElementById('burg');
+    let hiddenMenu = document.querySelector('.hidden');
+    let hiddenNav = document.querySelectorAll('.hidden__nav');
+
+    burg.addEventListener('click', function() {
+      hiddenNav.forEach(function(e) {
+        e.classList.toggle('opacity');
+      });
+      hiddenMenu.classList.toggle('hidden-visible');
+    });
+
+    hiddenNav.forEach(function(e) {
+      e.addEventListener('click', function() {
+        hiddenNav.forEach(function(e) {
+          e.classList.toggle('opacity');
+        });
+        hiddenMenu.classList.toggle('hidden-visible');
+      });
     });
     
 });
